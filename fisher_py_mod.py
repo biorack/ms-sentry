1c4c27a4985047d3f697581f68c538c38fe8d0e4
tharwood3 <tharwood@lbl.gov>
2022-05-02 15:00:59 -0700

    documentation

--- fisher_py_mod.py
+++ fisher_py_mod.py
@@ -1,13 +1,14 @@
-#Raw Class has been adapted from the RawFile class in fisher_py. Primarily so that .raw files can be closed after opening them
+#Fisher-py does not include a method for closing open file handles in the 'RawFile' class. These functions remedy that.
 
 from fisher_py.raw_file_reader.raw_file_access import RawFileAccess
 from fisher_py import RawFile
 
-
 def close_raw_file(self):
+	"""close .raw files"""
 	self._raw_file_access.dispose()
 	
 def assign_close_method():
+	"""assign method 'close_raw_file' to RawFile class"""
 	RawFile.close_raw_file = close_raw_file
 
 
